88c0dd925030efa599ef8955377fddadd5d9d7f8
jithin-nair <jithin_a@outlook.com>
2018-09-19 15:25:15 +0530

    Added 'export_keys' feature to Tychecash Simplewallet Cli
    Show the secret keys of the opened wallet
    1. Private Spend Key
    2. Private View Key

--- src/SimpleWallet/SimpleWallet.h
+++ src/SimpleWallet/SimpleWallet.h
@@ -71,6 +71,7 @@ namespace TycheCash
     bool start_mining(const std::vector<std::string> &args);
     bool stop_mining(const std::vector<std::string> &args);
     bool show_balance(const std::vector<std::string> &args = std::vector<std::string>());
+    bool export_keys(const std::vector<std::string> &args = std::vector<std::string>());
     bool show_incoming_transfers(const std::vector<std::string> &args);
     bool show_payments(const std::vector<std::string> &args);
     bool show_blockchain_height(const std::vector<std::string> &args);
